a5f68aaedc8c8b144c1163748f5f24593e2a0cde
pedroarias <pdroariasvilela@gmail.com>
2024-07-11 12:30:44 -0500

    chore:add config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://pdroariasvilela.github.io/portfolio-pedroariasvilela/"
 })
