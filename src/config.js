fa58a342c6b58fc61d85afd80d6f69dabc20e465
“pedroarias <pedroariasvilela@gmail.com>
2024-07-15 11:06:43 -0500

    update config

--- src/config.js
+++ src/config.js
@@ -13,8 +13,8 @@ export const information = [{
     jobs: {
         frontend: ["React.js", "React Native", "JavaScript", "Tailwindcss", "Next"],
         backend: ["Prisma", "Docker"],
-        descriptionSpanish: "Hola, soy Pedro Arias, desarrollador web y móvil con React y React Native. Con 6 meses de experiencia, he creado sitios web responsivos y aplicaciones móviles. ¡Colaboremos para crear algo increíble!",
-        descriptionEnglish: "Hi, I'm Pedro Arias, a React web and mobile developer with 6 months of experience. I've built responsive websites and mobile apps. Let's create something amazing together!"
+        descriptionSpanish: "Hola, soy Pedro Arias, un desarrollador web y móvil con 1 año de experiencia. Trabajo principalmente con React y React Native, y he construido sitios web responsivos y aplicaciones móviles utilizando estas tecnologías. ¡Vamos a crear algo increíble juntos!",
+        descriptionEnglish: "Hi, I'm Pedro Arias, a web and mobile developer with 1 year of experience. I primarily work with React and React Native, and I've built responsive websites and mobile apps using these technologies. Let's create something amazing together!"
     },
     projects:[
         {
